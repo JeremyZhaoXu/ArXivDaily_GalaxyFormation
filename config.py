1abb63793e698afdba33ba0aa79d78e310b2a8d5
Xu Zhao <53965350+JeremyZhaoXu@users.noreply.github.com>
2025-06-29 11:44:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = 'olozhika'
+USERNAME = 'JeremyZhaoXu'
 
 # The repository to add this issue to
-REPO_OWNER = 'olozhika'
-REPO_NAME = 'ArXivDaily_StarFormation'
+REPO_OWNER = 'JeremyZhaoXu'
+REPO_NAME = 'ArXivDaily_GalaxyFormation'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
-KEYWORD_LIST = ["star formation", "star-forming", "molecular cloud", "interstellar medium", "cloud", "clump", "core", "filament", "atomic gas", "N-PDF"]
+KEYWORD_LIST = ["galaxy formation", "satellite galaxy", "Milky Way", "simulation", "tidal disruption", "small-scale structure", "dwarf galaxy"]
 # Keywords to exclude
-KEYWORD_EX_LIST = ["galaxies", "galaxy cluster", " AGN ", "standard candle", "X-ray binar", "solar corona"]
+KEYWORD_EX_LIST = ["interstellar medium", "molecular cloud", "AGN", "standard candle", "X-ray binar", "solar corona", "Instrumentation"]
 # Note that the 'Keywords' above are actually searched in the abstract instead of the real keyword section. 
