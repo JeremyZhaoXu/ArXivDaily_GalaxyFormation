c432069ea67166dd893ddf2f52623989a3d721da
Xu Zhao <53965350+JeremyZhaoXu@users.noreply.github.com>
2025-06-29 17:10:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -110,6 +110,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get the TOKEN for github issue')
     parser.add_argument('-t','--token', help='The github TOKEN', required=True, default='Token Needed!')
     args = vars(parser.parse_args())
-    print(args['token'])
-    print(os.environ['TOKEN'])
+    #print(args['token'])
+    #print(os.environ['TOKEN'])
     main(args)
