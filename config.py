7b423ef43a4332de2e7494d4613b068a4e8d04f2
Xu Zhao <53965350+JeremyZhaoXu@users.noreply.github.com>
2025-06-30 13:34:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ REPO_NAME = 'ArXivDaily_GalaxyFormation'
 NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
-KEYWORD_LIST = ["galaxy formation", "satellite galaxy", "Milky Way", "simulation", "tidal disruption", "small-scale structure", "dwarf galaxy"]
+KEYWORD_LIST = ["galaxy formation", "satellite galaxy", "Milky Way", "TNG", "EAGLE", "tidal disruption", "small-scale structure", "dwarf galaxy", "N-body simulation", "hydrodynamical simulation", "cosmological simulation", "dark matter halos"]
 # Keywords to exclude
-KEYWORD_EX_LIST = ["interstellar medium", "molecular cloud", "AGN", "standard candle", "X-ray binar", "solar corona", "Instrumentation"]
+KEYWORD_EX_LIST = ["interstellar medium", "molecular cloud", "standard candle", "X-ray binar", "solar corona", "Instrumentation", "planet"]
 # Note that the 'Keywords' above are actually searched in the abstract instead of the real keyword section. 
